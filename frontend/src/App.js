117f56b47264274dc7652827985cd40059a9d56d
Lilholt <63080594+mrlilholt@users.noreply.github.com>
2024-12-17 15:05:05 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,7 @@ function App() {
       {/* Logo */}
       <Box sx={{ display: "flex", justifyContent: "center", marginBottom: 2 }}>
         <img
-          src="mushroomLogo.png" // Replace with your logo path
+          src="mushroomRecipe/mushroomLogo.png" // Replace with your logo path
           alt="Mushroom Recipe Logo"
           style={{ width: "80px", height: "auto" }}
         />
